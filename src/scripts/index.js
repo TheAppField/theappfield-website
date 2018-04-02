f09c3f77f9f8b683e9a098b4db54dac5bdd4e21d
Roman <roman@muehlfeldner.net>
2018-04-02 20:40:46 +0200

    Added footer. Included contact form in footer.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -61,10 +61,18 @@ const timelineAnimatons = function () {
         $(tiles[idx]).removeClass('is-hidden').addClass('fade-in');
       }
     });
+
     contactFields.each(function (idx) {
-      if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.75 && $(contactFields[idx]).hasClass('is-hidden')) {
-        $(contactFields[idx]).removeClass('is-hidden').addClass('fade-in');
+      if (idx !== contactFields.length - 1) {
+        if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.75 && $(contactFields[idx]).hasClass('is-hidden')) {
+          $(contactFields[idx]).removeClass('is-hidden').addClass('fade-in');
+        }
+      } else {
+        if ($(this).offset().top <= $(window).scrollTop() + $(window).height() && $(contactFields[idx]).hasClass('is-hidden')) {
+          $(contactFields[idx]).removeClass('is-hidden').addClass('fade-in');
+        }
       }
+
     });
   });
 };
@@ -73,83 +81,3 @@ $('document').ready(() => {
   // headerShrinker();
   timelineAnimatons();
 });
-
-// import 'bootstrap';
-// import ScrollReveal from 'scrollreveal';
-//
-// const css = require('../scss/main.scss');
-//
-// (function ($) {
-//     "use strict"; // Start of use strict
-//
-//     // Smooth scrolling using jQuery easing
-//     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-//         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-//             var target = $(this.hash);
-//             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//             if (target.length) {
-// (function ($) {
-//     "use strict"; // Start of use strict
-//
-//     // Smooth scrolling using jQuery easing
-//     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-//         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-//             var target = $(this.hash);
-//             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//             if (target.length) {
-//                 $('html, body').animate({
-//                     scrollTop: (target.offset().top - 48)
-//                 }, 1000, "easeInOutExpo");
-//                 return false;
-//             }
-//         }
-//     });
-//
-//     // Closes responsive menu when a scroll trigger link is clicked
-//     $('.js-scroll-trigger').click(func
-//                 $('html, body').animate({
-//                     scrollTop: (target.offset().top - 48)
-//                 }, 1000, "easeInOutExpo");
-//                 return false;
-//             }
-//         }
-//     });
-//
-//     // Closes responsive menu when a scroll trigger link is clicked
-//     $('.js-scroll-trigger').click(function () {
-//         $('.navbar-collapse').collapse('hide');
-//     });
-//
-//     // Activate scrollspy to add active class to navbar items on scroll
-//     $('body').scrollspy({
-//         target: '#mainNav',
-//         offset: 48
-//     });
-//
-//     // Collapse the navbar when page is scrolled
-//     $(window).scroll(function () {
-//         if ($("#mainNav").offset().top > 100) {
-//             $("#mainNav").addClass("navbar-shrink");
-//         } else {
-//             $("#mainNav").removeClass("navbar-shrink");
-//         }
-//     });
-//
-//     // Scroll reveal calls
-//     window.sr = ScrollReveal();
-//     sr.reveal('.sr-icons', {
-//         duration: 600,
-//         scale: 0.3,
-//         distance: '0px'
-//     }, 200);
-//     sr.reveal('.sr-button', {
-//         duration: 1000,
-//         delay: 200
-//     });
-//     sr.reveal('.sr-contact', {
-//         duration: 600,
-//         scale: 0.3,
-//         distance: '0px'
-//     }, 300);
-//
-// })(jQuery); // End of use strict
